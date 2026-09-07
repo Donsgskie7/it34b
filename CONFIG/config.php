ce8746842962670180ea20bdbaab0972047cb938
Donsgskie7 <johnanderson312006@gmail.com>
2026-09-07 15:43:42 +0800

    Added Initial RBAC

--- CONFIG/config.php
+++ CONFIG/config.php
@@ -7,7 +7,7 @@ require_once(__DIR__ . '/../includes/activity-logger.php');
 define('BASE_URL','http://localhost/IT34B');
 
 define('DB_HOST','localhost');
-define('DB_NAME','it34b_lab');
+define('DB_NAME','it34_lab_db');
 define('DB_USER','root');
 define('DB_PASS','');
 
